7f49bf4890f83e1d3da342705a0bae8576bfe1dc
clamchowder <lamchester@gmail.com>
2023-12-09 01:31:03 -0800

    r77

--- src/setup.cpp
+++ src/setup.cpp
@@ -628,9 +628,9 @@ void main_setup() { // DCS Missiles; required extensions in defines.hpp: FP16S, 
 	// ###################################################################################### define geometry ######################################################################################
 	const float size = 3.0f * lbm.size().x;
 	const float3 center = float3(lbm.center().x, 0.72f * size, lbm.center().z);
-	const float3x3 rotation = float3x3(float3(0, 0, 1), radians(-90.0f)); // for dcs
+	const float3x3 rotation = float3x3(float3(0, 1, 0), radians(-45.0f)) * float3x3(float3(0, 0, 1), radians(-90.0f)); // for dcs
 
-	lbm.voxelize_stl(get_exe_path()+"../stl/dcs_r40r.stl", center, rotation, size);
+	lbm.voxelize_stl(get_exe_path()+"../stl/dcs_r77.stl", center, rotation, size);
 	const uint Nx=lbm.get_Nx(), Ny=lbm.get_Ny(), Nz=lbm.get_Nz(); for(ulong n=0ull; n<lbm.get_N(); n++) { uint x=0u, y=0u, z=0u; lbm.coordinates(n, x, y, z);
 		if(lbm.flags[n]!=TYPE_S) lbm.u.y[n] = lbm_u;
 		if(x==0u||x==Nx-1u||y==0u||y==Ny-1u||z==0u||z==Nz-1u) lbm.flags[n] = TYPE_E; // all non periodic
